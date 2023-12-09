1540836c555ed0e941713eb5ab8b4877ccaa68cc
edwin <e.e.vos@hotmail.com>
2023-12-09 17:07:31 +0100

    Started cursusfiles chatscreen and chatclient

---
+++ src/app/chat-client/chat-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-client.component.html',
+  styleUrl: './chat-client.component.scss'
+})
+export class ChatClientComponent {
+
+}
---
+++ src/app/chat-screen/chat-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-screen.component.html',
+  styleUrl: './chat-screen.component.scss'
+})
+export class ChatScreenComponent {
+
+}
