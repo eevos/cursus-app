a0d774017a0a3b5de6083c01b42d8be9bde934c8
edwin <e.e.vos@hotmail.com>
2023-12-11 19:43:05 +0100

    Fixed bug on chat-client-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ChatClientComponent } from "./chat-client/chat-client.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, ChatClientComponent]
 })
 export class AppComponent {
   title = 'cursus-tape';
